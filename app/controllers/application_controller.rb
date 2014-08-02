fb95feca01b5baeb15012c38ac80e951e728d124
Michael Deering <mdeering@mdeering.com>
2014-08-02 15:35:38 -0600

    minor clean up before getting moving.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# 99% of your controllers in your application should have this controller in its inheritance chain.
+# So do things here like enforce security by default, always check for authroization and those
+# types of things.
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# These helpers get included in every view rendering that occures through any controller in the
+# application so don't start jamming this full of methods that only once controller and action
+# pair whould need.
 module ApplicationHelper
 end
